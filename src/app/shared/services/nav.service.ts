867cda6d7e6802adeee5e8ffe43e92eeade33348
MazenBoulares <Mazen.boulares@gmail.com>
2024-05-17 23:11:23 +0100

    restyling tempalte

--- src/app/shared/services/nav.service.ts
+++ src/app/shared/services/nav.service.ts
@@ -32,19 +32,19 @@ export class NavService {
       level: 1,
       path: '/dashboard'
     },
-    {
-      title: 'My properties',
-      icon: 'grid',
-      type: 'sub',
-      active: false,
-      level: 1,
-      children: [
-        { path: '/property/add-property', title: 'Add Property', type: 'link',level: 2, icon: 'chevrons-right' },
-        { path: '/property/edit-property', title: 'Edit Property', type: 'link',level: 2 , icon: 'chevrons-right'},
-        { path: '/property/property-list', title: 'Property List', type: 'link',level: 2 , icon: 'chevrons-right'},
-        { path: '/property/favourites', title: 'Favourites', type: 'link',level: 2 , icon: 'chevrons-right'},
-      ],
-    },
+    // {
+    //   title: 'My properties',
+    //   icon: 'grid',
+    //   type: 'sub',
+    //   active: false,
+    //   level: 1,
+    //   children: [
+    //     { path: '/property/add-property', title: 'Add Property', type: 'link',level: 2, icon: 'chevrons-right' },
+    //     { path: '/property/edit-property', title: 'Edit Property', type: 'link',level: 2 , icon: 'chevrons-right'},
+    //     { path: '/property/property-list', title: 'Property List', type: 'link',level: 2 , icon: 'chevrons-right'},
+    //     { path: '/property/favourites', title: 'Favourites', type: 'link',level: 2 , icon: 'chevrons-right'},
+    //   ],
+    // },
     {
       title: 'Manage users',
       icon: 'users',
@@ -59,58 +59,58 @@ export class NavService {
         { path: '/manage-user/all-user', title: 'All users', type: 'link',level: 2 , icon: 'chevrons-right'},
       ],
     },
-    {
-      title: 'Agents',
-      icon: 'user-plus',
-      type: 'sub',
-      active: false,
-      level: 1,
-      children: [
-        { path: '/agents/agent-profile', title: 'Profile', type: 'link',level: 2 , icon: 'chevrons-right'},
-        { path: '/agents/add-agent', title: 'Add agent', type: 'link',level: 2 , icon: 'chevrons-right'},
-        { path: '/agents/add-agent-wizard', title: 'Add agent wizard', type: 'link',level: 2 , icon: 'chevrons-right', badge: true, badgeText: 'New'},
-        { path: '/agents/edit-agent', title: 'Edit agent', type: 'link',level: 2 , icon: 'chevrons-right'},
-        { path: '/agents/all-agent', title: 'All agents', type: 'link',level: 2 , icon: 'chevrons-right'},
-        { path: '/agents/invoice', title: 'Invoice', type: 'link',level: 2 , icon: 'chevrons-right'},
-      ],
-    },
-    {
-      title: 'Map',
-      icon: 'map-pin',
-      type: 'link',
-      active: false,
-      level: 1,
-      path: '/map'
-    },
-    {
-      title: 'Types',
-      icon: 'layout',
-      type: 'sub',
-      active: false,
-      level: 1,
-      children: [
-        { path: '/type/family-house', title: 'Family House', type: 'link',level: 2 , icon: 'chevrons-right'},
-        { path: '/type/cottage', title: 'Cottage', type: 'link',level: 2 , icon: 'chevrons-right'},
-        { path: '/type/apartment', title: 'Apartment', type: 'link',level: 2 , icon: 'chevrons-right'},
-        { path: '/type/condominium', title: 'Condominium', type: 'link',level: 2 , icon: 'chevrons-right'}
-      ],
-    },
-    {
-      title: 'Reports',
-      icon: 'bar-chart-2',
-      type: 'link',
-      active: false,
-      level: 1,
-      path: '/reports'
-    },
-    {
-      title: 'Payments',
-      icon: 'credit-card',
-      type: 'link',
-      active: false,
-      level: 1,
-      path: '/payments'
-    },
+    // {
+    //   title: 'Agents',
+    //   icon: 'user-plus',
+    //   type: 'sub',
+    //   active: false,
+    //   level: 1,
+    //   children: [
+    //     { path: '/agents/agent-profile', title: 'Profile', type: 'link',level: 2 , icon: 'chevrons-right'},
+    //     { path: '/agents/add-agent', title: 'Add agent', type: 'link',level: 2 , icon: 'chevrons-right'},
+    //     { path: '/agents/add-agent-wizard', title: 'Add agent wizard', type: 'link',level: 2 , icon: 'chevrons-right', badge: true, badgeText: 'New'},
+    //     { path: '/agents/edit-agent', title: 'Edit agent', type: 'link',level: 2 , icon: 'chevrons-right'},
+    //     { path: '/agents/all-agent', title: 'All agents', type: 'link',level: 2 , icon: 'chevrons-right'},
+    //     { path: '/agents/invoice', title: 'Invoice', type: 'link',level: 2 , icon: 'chevrons-right'},
+    //   ],
+    // },
+    // {
+    //   title: 'Map',
+    //   icon: 'map-pin',
+    //   type: 'link',
+    //   active: false,
+    //   level: 1,
+    //   path: '/map'
+    // },
+    // {
+    //   title: 'Types',
+    //   icon: 'layout',
+    //   type: 'sub',
+    //   active: false,
+    //   level: 1,
+    //   children: [
+    //     { path: '/type/family-house', title: 'Family House', type: 'link',level: 2 , icon: 'chevrons-right'},
+    //     { path: '/type/cottage', title: 'Cottage', type: 'link',level: 2 , icon: 'chevrons-right'},
+    //     { path: '/type/apartment', title: 'Apartment', type: 'link',level: 2 , icon: 'chevrons-right'},
+    //     { path: '/type/condominium', title: 'Condominium', type: 'link',level: 2 , icon: 'chevrons-right'}
+    //   ],
+    // },
+    // {
+    //   title: 'Reports',
+    //   icon: 'bar-chart-2',
+    //   type: 'link',
+    //   active: false,
+    //   level: 1,
+    //   path: '/reports'
+    // },
+    // {
+    //   title: 'Payments',
+    //   icon: 'credit-card',
+    //   type: 'link',
+    //   active: false,
+    //   level: 1,
+    //   path: '/payments'
+    // },
     {
       title: 'Authentication',
       icon: 'unlock',
